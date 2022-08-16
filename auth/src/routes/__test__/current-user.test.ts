74e3e3fb36fd5076e5a0b454bec024411b92b9ce
Okwuidegbe Emmanuel <okwuidegbeemmanuel@gmail.com>
2022-08-16 21:58:58 +0100

    Test current-user route

---
+++ auth/src/routes/__test__/current-user.test.ts
@@ -0,0 +1,25 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it('should respond with details about current user', async () => {
+  process.env.MY_SECRETS = 'asdfqwer'
+
+  const authRes = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(201)
+  
+  const cookie = authRes.get('Set-Cookie')
+
+  const res = await request(app)
+    .get('/api/users/currentuser')
+    .set('Cookie', cookie)
+    .send()
+    .expect(200)
+
+  expect(res.body.currentUser.email).toEqual('test@test.com')
+})
+
