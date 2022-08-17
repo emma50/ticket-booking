bf301984a15ece83887b83954e2cffd9d86bc8be
Okwuidegbe Emmanuel <okwuidegbeemmanuel@gmail.com>
2022-08-17 08:31:29 +0100

    Set auth helper for testing

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -4,16 +4,16 @@ import { app } from "../../app";
 it('should respond with details about current user', async () => {
   process.env.MY_SECRETS = 'asdfqwer'
 
-  const authRes = await request(app)
-    .post('/api/users/signup')
-    .send({
-      email: 'test@test.com',
-      password: 'password'
-    })
-    .expect(201)
+  // const authRes = await request(app)
+  //   .post('/api/users/signup')
+  //   .send({
+  //     email: 'test@test.com',
+  //     password: 'password'
+  //   })
+  //   .expect(201)
   
-  const cookie = authRes.get('Set-Cookie')
-
+  // const cookie = authRes.get('Set-Cookie')
+  const cookie = await signup()
   const res = await request(app)
     .get('/api/users/currentuser')
     .set('Cookie', cookie)
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,11 @@
 import { MongoMemoryServer } from "mongodb-memory-server"
 import mongoose from "mongoose";
+import request from 'supertest'
 import { app } from "../app";
+
+declare global {
+  var signup: () => Promise<string[]>;
+}
 
 let mongo: any;
 beforeAll(async() => {
@@ -26,3 +31,17 @@ afterAll(async() => {
   await mongoose.connection.close()
 }, 300000)
 
+
+global.signup = async () => {
+  const email = 'test@test.com'
+  const password = 'password'
+
+  const res = await request(app)
+    .post('/api/users/signup')
+    .send({ email, password })
+    .expect(201)
+
+    const cookie = res.get('Set-Cookie')
+
+    return cookie
+}
