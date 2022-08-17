8bc6015d3ba8d1cb358d149524a2231f4c159874
Okwuidegbe Emmanuel <okwuidegbeemmanuel@gmail.com>
2022-08-17 11:21:43 +0100

    Test non authenticated routes

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -4,15 +4,6 @@ import { app } from "../../app";
 it('should respond with details about current user', async () => {
   process.env.MY_SECRETS = 'asdfqwer'
 
-  // const authRes = await request(app)
-  //   .post('/api/users/signup')
-  //   .send({
-  //     email: 'test@test.com',
-  //     password: 'password'
-  //   })
-  //   .expect(201)
-  
-  // const cookie = authRes.get('Set-Cookie')
   const cookie = await signup()
   const res = await request(app)
     .get('/api/users/currentuser')
@@ -23,3 +14,14 @@ it('should respond with details about current user', async () => {
   expect(res.body.currentUser.email).toEqual('test@test.com')
 })
 
+it('should respond with null if not authenticated', async () => {
+  process.env.MY_SECRETS = 'asdfqwer'
+
+  const res = await request(app)
+    .get('/api/users/currentuser')
+    .send()
+    .expect(200)
+
+  console.log(res.body.currentUser, 'RESTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT')
+  expect(res.body.currentUser).toEqual(null)
+})
