8f9839bdec8dbb59663a2e38c023761ff4b7de72
Okwuidegbe Emmanuel <okwuidegbeemmanuel@gmail.com>
2022-08-22 12:49:58 +0100

    building a reusable api client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import buildClient from '../api/build-client'
 
 // code executed on the browser
 const Landing = ({ currentUser }) => {
@@ -8,35 +8,10 @@ const Landing = ({ currentUser }) => {
 
 // code executed on the server during the SSR process
 // or the browser during the CSR process
-Landing.getInitialProps = async ({ req }) => {
-  console.log(req.headers)
-  if (typeof window === 'undefined') {
-    // code executed on the server
-    console.log('on server')
-    try {
-      // request must be made with the server's domain name
-      // i.e http://SERVICENAME.NAMESPACE.svc.cluster.local
-      const res = await axios.get(
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
-          headers: req.headers
-        }
-      )
-      return res.data;
-    } catch (err) {
-      console.log(err)
-    }
-  } else {
-    // code executed on the browser
-    console.log('on browser')
-    try {
-      // request can be made with a base url of ''
-      const res = await axios.get('/api/users/currentuser')
-      return res.data;
-    } catch (err) {
-      console.log(err)
-    }
-  }
-  return {}
+Landing.getInitialProps = async (context) => {
+  const client = buildClient(context)
+  const res = await client.get('/api/users/currentuser')
+  return res.data
 }
 
 export default Landing
