29af9c6bd5ad5820af940332e4033dcac4f3aa26
Okwuidegbe Emmanuel <okwuidegbeemmanuel@gmail.com>
2022-08-23 07:39:47 +0100

    Passing props through Components - The entry point App Component - The individual components

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,6 +1,28 @@
 import "bootstrap/dist/css/bootstrap.css"
+import buildClient from "../api/build-client"
 
 // nextjs custom component --> A wrapper around all components
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+ const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <h1>Header! {currentUser && currentUser.email}</h1>
+      <Component {...pageProps} />
+    </div>
+  )
 }
+
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx)
+  const res = await client.get('/api/users/currentuser')
+  
+  let pageProps = {}
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx)
+  }
+  return {
+    pageProps,
+    currentUser: res.data.currentUser
+  }
+}
+
+export default AppComponent
