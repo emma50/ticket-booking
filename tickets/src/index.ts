45d17251a7eaf7a2f98361fd7354b93ae4b0ecab
Okwuidegbe Emmanuel <okwuidegbeemmanuel@gmail.com>
2022-08-25 13:17:18 +0100

    Setup tickets micro-service - Setup docker image - Setup kubernetes for ticket service - Setup skaffold for automated ticket service kubernetes development

---
+++ tickets/src/index.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import { app } from "./app";
+
+const start = async () => {
+  if (!process.env.MY_SECRETS) {
+    throw new Error('All secrets must be defined')
+  }
+
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined')
+  }
+  try {
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log('Connected to ticket mongodb')
+  } catch (err) {
+    console.error(err)
+  } 
+  app.listen(3000, () => {
+    console.log('Server up at port 3000')
+    console.log('Listening at port 3000')
+  })
+}
+
+start()
---
+++ tickets/src/test/setup.ts
@@ -0,0 +1,48 @@
+import { MongoMemoryServer } from "mongodb-memory-server"
+import mongoose from "mongoose";
+import request from 'supertest'
+import { app } from "../app";
+
+jest.setTimeout(400000)
+declare global {
+  var signup: () => Promise<string[]>;
+}
+
+let mongo: any;
+beforeAll(async() => {
+  // Create a mongo memory server connection before running all tests
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri()
+
+  await mongoose.connect(mongoUri, {})
+})
+
+beforeEach(async() => {
+  // Delete all collections for each test operation
+  const collections = await mongoose.connection.db.collections();
+  
+  for (let collection of collections) {
+    await collection.deleteMany({})
+  }
+})
+
+afterAll(async() => {
+  // Stop and close connection to mongodb
+  await mongo.stop()
+  await mongoose.connection.close()
+})
+
+
+global.signup = async () => {
+  const email = 'test@test.com'
+  const password = 'password'
+
+  const res = await request(app)
+    .post('/api/users/signup')
+    .send({ email, password })
+    .expect(201)
+
+    const cookie = res.get('Set-Cookie')
+
+    return cookie
+}
