d567681954d792906277810c636b7e33c106c7a7
Okwuidegbe Emmanuel <okwuidegbeemmanuel@gmail.com>
2022-09-02 14:49:34 +0100

    TDD (Test Driven Development) - setup a create ticket router

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,6 +3,8 @@ import 'express-async-errors'
 import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError } from '@e50tickets/common'
 
+import { createTicketRouter } from "./routes/new";
+
 const app = express()
 
 app.use(express.json())
@@ -12,6 +14,7 @@ app.use(cookieSession({
   signed: false,
   secure: process.env.NODE_ENV !== 'test',
 }))
+app.use(createTicketRouter)
 app.all('*', async () => {
   throw new NotFoundError()
 })
--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -2,7 +2,11 @@ import request from 'supertest'
 import { app } from '../../app'
 
 it('has a route handler listening to /api/tickets for a POST request', async () => {
-  //
+  const res = await request(app)
+    .post('/api/tickets')
+    .send({})
+  console.log(res.status)
+  expect(res.status).not.toEqual(404)
 })
 
 it('/api/tickets can only be accessed if the user is signed in', async () => {
