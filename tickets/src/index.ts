ad374bbe9a576b94ca46d8b35297bb2fa3f30f85
Okwuidegbe Emmanuel <okwuidegbeemmanuel@gmail.com>
2022-10-09 22:23:11 +0100

    Update tickets-srv

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import { app } from "./app";
 import { natsWrapper } from "./nats-wrapper";
+import { OrderCreatedListener } from "./events/listeners/order-created-listener";
+import { OrderCancelledListener } from "./events/listeners/order-cancelled-listener";
 
 const start = async () => {
   if (!process.env.MY_SECRETS) {
@@ -30,6 +32,10 @@ const start = async () => {
       process.env.NATS_URL
     )
     natsWrapper.close()
+
+    new OrderCreatedListener(natsWrapper.client).listen()
+    new OrderCancelledListener(natsWrapper.client).listen()
+
     await mongoose.connect(process.env.MONGO_URI)
     console.log('Connected to ticket mongodb')
   } catch (err) {
