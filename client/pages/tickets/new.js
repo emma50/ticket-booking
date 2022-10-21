54ad66b80d4705d14a3e8588d17c123092da33d0
Okwuidegbe Emmanuel <okwuidegbeemmanuel@gmail.com>
2022-10-21 09:47:45 +0100

    Client srv: Sanitize price input

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -1,15 +1,44 @@
+import { useState } from "react";
+
 const NewTicket = () => {
+  const [title, setTitle] = useState('')
+  const [price, setPrice] = useState('')
+  console.log(title, price)
+
+  const onBlur = () => {
+    const value = parseFloat(price)
+
+    if (isNaN(value)) {
+      return;
+    }
+
+    setPrice(value.toFixed(2))
+  }
+
   return (
     <div>
       <h1>Create a new ticket</h1>
       <form>
         <div className="form-group">
           <label htmlFor="">Title:</label>
-          <input type="text" className="form-control"/>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            type="text"
+            className="form-control"
+            placeholder="e.g movie"
+          />
         </div>
         <div className="form-group">
           <label htmlFor="">Price:</label>
-          <input type="text" className="form-control"/>
+          <input
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+            onBlur={onBlur}
+            type="text"
+            className="form-control"
+            placeholder="e.g $20"
+          />
         </div>
         <button type="submit" className="btn btn-primary">Submit</button>
       </form>
