8631c5d7ee0261b6f695900864290cbfc95cfbb8
Okwuidegbe Emmanuel <okwuidegbeemmanuel@gmail.com>
2022-10-25 07:56:05 +0100

    Client srv: Implement expiration timer

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,9 +1,46 @@
-const OrderShow = () => {
+import { useState, useEffect } from 'react'
+
+const OrderShow = ({ order }) => {
+  const [timeLeft, setTimeLeft] = useState('')
+
+  useEffect(() => {
+    const findTimeLeft = () => {
+      const msLeft = new Date(order.expiresAt) - new Date()
+      // msLeft / 1000 converts millisecs to secs
+      setTimeLeft(Math.round(msLeft / 1000))
+    }
+
+    // Manually invoke findTimeLeft immediately
+    findTimeLeft()
+
+    const timerId = setInterval(findTimeLeft , 1000)
+
+    // Invoked when navigating from the component
+    return () => {
+      clearInterval(timerId)
+    }
+  }, [order])
+
+  console.log(order)
+  console.log(order.expiresAt, 'ORDER')
+  console.log(new Date(order.expiresAt), 'NEWDATE')
+  console.log(new Date(), 'DATEE')
+
   return (
     <div>
-      RouterShow
+      {timeLeft} seconds until order expires
     </div>
   )
+}
+
+OrderShow.getInitialProps = async (context, client) => {
+  // Extracting the orderId query (because this file is called orderId)
+  const { orderId } = context.query
+  const res = await client.get(`/api/orders/${orderId}`)
+
+  return {
+    order: res.data.data
+  }
 }
 
 export default OrderShow
